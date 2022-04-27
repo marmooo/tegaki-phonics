bda28d67d073ae5986b62c48a8ce441155a103c7
marmooo <marmooo@users.noreply.github.com>
2022-04-28 00:04:45 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-23 08:06';
+var CACHE_NAME = '2022-04-28 00:05';
 var urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/eraser.svg",
@@ -7,7 +7,7 @@ var urlsToCache = [
   "/tegaki-phonics/model/group1-shard1of1.bin",
   "/tegaki-phonics/mp3/end.mp3",
   "/tegaki-phonics/mp3/correct3.mp3",
-  "/tegaki-phonics/favicon/original.svg",
+  "/tegaki-phonics/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.16.0/dist/tf.min.js",
