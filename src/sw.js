83696bf5d14dd11b2183da1f43aad7851983a481
marmooo <marmooo@users.noreply.github.com>
2022-05-21 18:09:28 +0900

    bump tfjs from 3.16.0 to 3.17.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-05-11 09:49";
+var CACHE_NAME = "2022-05-21 18:08";
 var urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/eraser.svg",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/tegaki-phonics/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.16.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.17.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
