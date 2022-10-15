5ee965537f31a59350d51796ca6c901e11914a65
marmooo <marmooo@users.noreply.github.com>
2022-10-15 12:59:14 +0900

    bump signature_pad from 4.0.9 to 4.0.10

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-09 10:18";
+var CACHE_NAME = "2022-10-15 12:58";
 var urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/index.js",
@@ -12,7 +12,7 @@ var urlsToCache = [
   "/tegaki-phonics/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.9/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.10/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.21.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
