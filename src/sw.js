ffeac76963efda59f17614b887ccfc9c4923fdb8
marmooo <marmooo@users.noreply.github.com>
2023-01-06 18:35:07 +0900

    bump tfjs from 4.1.0 to 4.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-12-30 15:20";
+var CACHE_NAME = "2023-01-06 18:34";
 var urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/index.js",
@@ -13,7 +13,7 @@ var urlsToCache = [
   "https://marmooo.github.io/yomico/yomico.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.4/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.1.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.2.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
