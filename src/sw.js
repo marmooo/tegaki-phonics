c725c8f311ca26d7f902761043432b887c7b9319
marmooo <marmooo@users.noreply.github.com>
2023-05-20 20:29:01 +0900

    remove animate.css

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-05-14 15:07";
+var CACHE_NAME = "2023-05-20 20:30";
 var urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/index.js",
@@ -12,7 +12,6 @@ var urlsToCache = [
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.5.0/dist/tf.min.js",
-  "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
 
