2ac87051f4beecf4bea3eda91ab979efc8e0975f
marmooo <marmooo@users.noreply.github.com>
2023-07-31 07:23:47 +0900

    cleanup

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = "2023-07-29 10:17";
-var urlsToCache = [
+const CACHE_NAME = "2023-07-31 07:25";
+const urlsToCache = [
   "/tegaki-phonics/",
   "/tegaki-phonics/index.js",
   "/tegaki-phonics/worker.js",
@@ -13,38 +13,28 @@ var urlsToCache = [
   "https://fonts.googleapis.com/css?family=Source+Code+Pro",
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      }),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
   );
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }),
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
   );
 });
 
-self.addEventListener("activate", function (event) {
-  var cacheWhitelist = [CACHE_NAME];
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        }),
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
       );
     }),
   );
