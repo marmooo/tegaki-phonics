e5fd6e04b0891fb4a31d79de5a112a106e6dccee
marmooo <marmooo@users.noreply.github.com>
2025-07-26 10:27:46 +0900

    improve loopVoice()

--- src/index.js
+++ src/index.js
@@ -163,12 +163,11 @@ function loadVoices() {
 
 function loopVoice(text, n) {
   speechSynthesis.cancel();
+  text = new Array(n).fill(`${text}.`).join(" ");
   const msg = new globalThis.SpeechSynthesisUtterance(text);
   msg.voice = englishVoices[Math.floor(Math.random() * englishVoices.length)];
   msg.lang = "en-US";
-  for (let i = 0; i < n; i++) {
-    speechSynthesis.speak(msg);
-  }
+  speechSynthesis.speak(msg);
 }
 
 function respeak() {
