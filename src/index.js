9cf9bf17ca16c04f34ad377491307305ec8aa764
marmooo <marmooo@users.noreply.github.com>
2025-09-09 00:35:41 +0900

    bump bootstrap from 5.3.7 to 5.3.8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/+esm";
+import { Collapse } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/+esm";
 import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.0/+esm";
 
 const playPanel = document.getElementById("playPanel");
